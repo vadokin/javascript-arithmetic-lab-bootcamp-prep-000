db11aa5f09f61f07e1e4b6df1515d0a8bbcf5a57
Tomislav Stamenovski <krypton.style@gmail.com>
2017-11-04 11:25:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b
 }
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n++
+}
+
+function dec(n) {
+  return n--
+}
+
+function 
